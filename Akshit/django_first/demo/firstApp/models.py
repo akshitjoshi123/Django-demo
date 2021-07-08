e4530bd320d9c347233a958eb38abc7fcd1775b1
akshitjoshi9@gmail.com <akshitjoshi9@gmail.com>
2021-07-08 19:01:20 +0530

    Models and Templates

--- Akshit/django_first/demo/firstApp/models.py
+++ Akshit/django_first/demo/firstApp/models.py
@@ -1,3 +1,13 @@
+from os import name
 from django.db import models
 
 # Create your models here.
+
+class index(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
--- Akshit/django_first/demo/firstApp/views.py
+++ Akshit/django_first/demo/firstApp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render,HttpResponse
+from .models import index
 
-# Create your views here.
-
+# Create your views here.s
 def first(request):
-    return HttpResponse("Hey i am Fisrt Demo app")
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = index(name=name, email=email, password=password)
+        user.save()
+    return render(request, 'index.html', {'name' : 'Akshit Joshi'})
